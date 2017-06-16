ebe2c2ab72bce467871c9bbbb3fd637886cd7507
Waroth <homanleung1@gmail.com>
2017-06-16 02:16:37 -0700

    Initial Commit

---
+++ Source/G_CPP/G_CPPGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "G_CPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class G_CPP_API AG_CPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
